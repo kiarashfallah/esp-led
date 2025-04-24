b906f5c51366b5eb0cbe7eb4d46697d6980a0b00
kiarashfallah <126492981+kiarashfallah@users.noreply.github.com>
2025-04-25 01:09:11 +0400

    app.py

--- app.py
+++ app.py
@@ -19,4 +19,9 @@ def get_led_status():
             state = json.load(f)
         return jsonify(state)
     except:
-        return jsonify({'led': 'off'})  # default
+        return jsonify({'led': 'off'})  # defaul
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
